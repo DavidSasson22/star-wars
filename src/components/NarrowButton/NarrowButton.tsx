31d843ae80e3f8320e989dc42bdd75870d978ba0
DavidSasson22 <75793795+DavidSasson22@users.noreply.github.com>
2021-08-09 16:33:27 +0300

    first fetch

---
+++ src/components/NarrowButton/NarrowButton.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NarrowButton() {
+  return (
+    <button>
+      hello
+    </button>
+  )
+}
--- src/components/Toc/Toc.tsx
+++ src/components/Toc/Toc.tsx
@@ -1,16 +1,40 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
+import NarrowButton from '../NarrowButton/NarrowButton';
 import "./toc.css"
 
 export default function Toc() {
-  const [displyToc, setDisplatToc] = useState("flex-wraper left-hiden");
+  const [displyToc, setDisplatToc] = useState("flex-wraper");
+  const [loader, setLoader] = useState(false);
+  const [error, setError] = useState(false);
+  const [data, setData] = useState([]);
+
+
+  useEffect(() => {
+    const getMovies = async () => {
+      setLoader(true);
+      try {
+        let movies = await (await axios.get(`https://swapi.dev/api/films/`)).data.results;
+        console.log(movies);
+
+      } catch (e) {
+        console.log(e);
+
+      }
+    }
+    getMovies();
+  }, [])
+
 
   const tocDisplayer = (): void => {
     displyToc === "flex-wraper" ? setDisplatToc("flex-wraper left-hiden") : setDisplatToc("flex-wraper")
   }
+
+
   return (
     <div className={displyToc}>
       <div className="table-of-content">
-
+        <NarrowButton />
       </div>
       <div className="table-displayer" onClick={() => tocDisplayer()}>
 
