364f38fe59c9910450ef7143124889082621eb0d
DavidSasson22 <75793795+DavidSasson22@users.noreply.github.com>
2021-08-09 12:50:12 +0300

    first installation + header

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './app.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Welcome to star wars encyclopedia</h1>
+        <h2>Everything you need to know about star wars movies</h2>
       </header>
     </div>
   );
