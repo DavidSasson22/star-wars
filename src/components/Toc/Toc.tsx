c35fc5477a67feee54427c6638fdd79616757570
DavidSasson22 <75793795+DavidSasson22@users.noreply.github.com>
2021-08-09 17:07:07 +0300

    added loader

--- src/components/Toc/Toc.tsx
+++ src/components/Toc/Toc.tsx
@@ -2,28 +2,31 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 import NarrowButton from '../NarrowButton/NarrowButton';
 import "./toc.css"
+import Loader from '../Loader/Loader';
 
 export default function Toc() {
   const [displyToc, setDisplatToc] = useState("flex-wraper");
   const [loader, setLoader] = useState(false);
   const [error, setError] = useState(false);
-  const [data, setData] = useState([]);
+  const [moviesData, setMoviesData] = useState([]);
 
 
   useEffect(() => {
     const getMovies = async () => {
       setLoader(true);
+      setError(false);
       try {
         let movies = await (await axios.get(`https://swapi.dev/api/films/`)).data.results;
         console.log(movies);
+        setMoviesData(moviesData)
 
       } catch (e) {
         console.log(e);
-
+        setError(true)
       }
     }
     getMovies();
-  }, [])
+  }, [moviesData])
 
 
   const tocDisplayer = (): void => {
@@ -37,8 +40,8 @@ export default function Toc() {
         <NarrowButton />
       </div>
       <div className="table-displayer" onClick={() => tocDisplayer()}>
-
       </div>
+      {loader && <Loader />}
     </div>
   )
 }
